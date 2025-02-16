b6db6fac7fb895278c002247af4222440d5beeec
Ernest Mikita <ernest.mikita@gmail.com>
2025-02-16 12:00:24 +0100

    Pre-built SDL Android files

---
+++ TheForceEngine/sdl2_android/include/SDL_revision.h
@@ -0,0 +1,8 @@
+/* #undef SDL_VENDOR_INFO */
+#define SDL_REVISION_NUMBER 0
+
+#ifdef SDL_VENDOR_INFO
+#define SDL_REVISION "SDL-v0.9.26-android-1-g76d99741 (" SDL_VENDOR_INFO ")"
+#else
+#define SDL_REVISION "SDL-v0.9.26-android-1-g76d99741"
+#endif
